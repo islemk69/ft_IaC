d89a323030710ccb347b18a51830e2efe50996eb
Laël <13titirex@pm.me>
2025-12-11 12:37:57 +0100

    fix: sensitive var

--- modules/autoscaling/variables.tf
+++ modules/autoscaling/variables.tf
@@ -14,5 +14,8 @@ variable "vpc_id" { type = string }
 variable "subnet_id" { type = string }
 variable "db_host" { type = string }
 variable "db_user" { type = string }
-variable "db_password" { type = string }
+variable "db_password" {
+   type = string
+   sensitive = true
+}
 variable "db_name" { type = string }
